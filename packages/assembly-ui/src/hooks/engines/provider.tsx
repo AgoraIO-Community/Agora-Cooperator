55236f553fe1deb30e4d638c8768b5f9e22ed0ff
xueqingxiao <xueqingxiao@agora.io>
2022-06-17 14:05:50 +0800

    feat: set screen capture scenario

--- packages/assembly-ui/src/hooks/engines/provider.tsx
+++ packages/assembly-ui/src/hooks/engines/provider.tsx
@@ -165,7 +165,7 @@ export const EnginesProvider: FC = ({ children }) => {
       );
     }
 
-    rtcEngine.instance.setScreenCaptureScenario(4);
+    rtcEngine.instance.videoSourceSetScreenCaptureScenario(4);
 
     instance
       .join(userId, token, session.channel, streamId, streamToken)
